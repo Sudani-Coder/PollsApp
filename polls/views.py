ac16372e0e82d17a18398bc6a4f84a5a41a56b1f
Sudani-Coder <sudani.coder@gmail.com>
2023-12-10 15:47:49 +0400

    write test code for models and views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from typing import Any
-from django.db.models.query import QuerySet
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -13,13 +12,20 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-    def get_queryset(self) -> QuerySet[Any]:
+    def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DeleteView):
     model = Question
