186960551e9a99fad6ae18200d4adb98cea57ebe
Sudani-Coder <sudani.coder@gmail.com>
2023-12-29 03:29:06 +0400

    insert dummy data using faker

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
